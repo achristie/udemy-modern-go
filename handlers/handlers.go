5cb18335226d79b6af64d52d9e3c6e5c35ef8a01
Andrew Christie <awchristie86@gmail.com>
2021-11-28 00:02:28 -0500

    using templates

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -27,6 +27,21 @@ func NewHandlers(r *Repository) {
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservations.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+func (m *Repository) Search(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
 
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
