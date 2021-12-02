49dc98a835ac3c0cf84d63a2d3e0514cb1b83a11
Andrew Christie <awchristie86@gmail.com>
2021-12-01 20:35:04 -0500

    added really brittle tests

---
+++ handlers/setup_test.go
@@ -0,0 +1,129 @@
+package handlers
+
+import (
+	"encoding/gob"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/achristie/udemy-modern-go/config"
+	"github.com/achristie/udemy-modern-go/models"
+	"github.com/achristie/udemy-modern-go/render"
+	"github.com/alexedwards/scs/v2"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/justinas/nosurf"
+)
+
+var functions = template.FuncMap{}
+var app config.AppConfig
+var session *scs.SessionManager
+var pathToTemplates = "../templates"
+
+func getRoutes() http.Handler {
+
+	gob.Register(models.Reservation{})
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	app.Session = session
+
+	tc, err := CreateTestTemplateCache()
+	if err != nil {
+		log.Fatalf("Cannot create template cache: %s", err)
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = true
+
+	repo := NewRepo(&app)
+	NewHandlers(repo)
+	render.NewTemplates(&app)
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
+
+	mux.Get("/", Repo.Home)
+	mux.Get("/about", Repo.About)
+	mux.Get("/generals-quarters", Repo.Generals)
+	mux.Get("/majors-suite", Repo.Majors)
+	mux.Get("/contact", Repo.Contact)
+
+	mux.Get("/make-reservations", Repo.Reservation)
+	mux.Post("/make-reservations", Repo.PostReservation)
+	mux.Get("/reservation-summary", Repo.ReservationHandler)
+
+	mux.Post("/search-availability", Repo.PostSearch)
+	mux.Get("/search-availability", Repo.Search)
+	mux.Post("/search-json", Repo.AvailJSON)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	return mux
+}
+
+func WriteToConsole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hit the page")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func NoSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+	return csrfHandler
+}
+
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
+}
+
+func CreateTestTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
+	if err != nil {
+		return cache, err
+	}
+
+	for _, pg := range pages {
+		name := filepath.Base(pg)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(pg)
+		if err != nil {
+			return cache, err
+		}
+
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
+		if err != nil {
+			return cache, err
+		}
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
+			if err != nil {
+				return cache, err
+			}
+
+			cache[name] = ts
+		}
+	}
+	return cache, nil
+}
